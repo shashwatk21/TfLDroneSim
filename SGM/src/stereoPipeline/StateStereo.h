bee9b1780b9c4c472e8309747a61e1a502154280
Shashwat <skansal41@gmail.com>
2023-06-23 13:11:19 +0100

    Add examples with stereo

---
+++ SGM/src/stereoPipeline/StateStereo.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "../sgmstereo/sgmstereo.h"
+#include "SGMOptions.h"
+#include <vector>
+
+class CStateStereo
+{
+private:
+    float downSampleFactor;
+    std::string outputDir;
+    int inputFrameWidth, inputFrameHeight;
+    int ndisps;
+    int minDisp;
+    int maxDisp;
+    int confThreshold;
+
+    SGMStereo* sgmStereo;
+
+public:
+    int processingFrameWidth, processingFrameHeight;
+
+    CStateStereo();
+    ~CStateStereo();
+    void Initialize(SGMOptions& params, int m = 144, int n = 256);
+    void CleanUp();
+    void ProcessFrameAirSim(int frameCounter, float& dtime, const std::vector<uint8_t>& left_image, const std::vector<uint8_t>& right_image);
+    float GetLeftDisparity(float x, float y);
+
+    float* dispMap;
+    unsigned char* confMap;
+};
